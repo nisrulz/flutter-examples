f0f31c27e9bff9c47d640d8b58188e9a348dd0d0
Nishant Srivastava <nisrulz@gmail.com>
2017-08-21 13:57:10 -0700

    cleanup

--- simple_material_app/lib/main.dart
+++ simple_material_app/lib/main.dart
@@ -2,11 +2,16 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(new MaterialApp(
+      // Title
       title: "Simple Material App",
+      // Home
       home: new Scaffold(
+        // Appbar
         appBar: new AppBar(
+          // Title
           title: new Text("Simple Material App"),
         ),
+        // Body
         body: new Container(
           child: new Center(
             child: new Text("Hello World!"),
