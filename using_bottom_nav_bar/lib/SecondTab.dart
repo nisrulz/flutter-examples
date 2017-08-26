a7252a9deccf014ac6112ab36c69b6ed0bdc6f11
Nishant Srivastava <nisrulz@gmail.com>
2017-08-26 00:34:29 -0700

    implemented using_bottom_nav_bar example

---
+++ using_bottom_nav_bar/lib/SecondTab.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Second extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Colors.green,
+      body: new Container(
+        child: new Center(
+          child: new Column(
+            // center the children
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Icon(
+                Icons.adb,
+                size: 160.0,
+                color: Colors.white,
+              ),
+              new Text(
+                "Second Tab",
+                style: new TextStyle(color: Colors.white),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
