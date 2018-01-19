f471bf11d1e067bc28d4f701c284eb136cb38fbb
Nishant Srivastava <nisrulz@gmail.com>
2018-01-19 23:05:08 +0530

    added: enabling_splash_screen example

---
+++ enabling_splash_screen/README.md
@@ -0,0 +1,8 @@
+# enabling_splash_screen
+
+Splash screen in a flutter project
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](http://flutter.io/).
---
+++ enabling_splash_screen/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Splash Screen Example"),
+        ),
+        body: new Center(
+          child: new Text("Hello World"),
+        ),
+      ),
+    );
+  }
+}
