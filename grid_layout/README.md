b2c80a698efe293baff79445d7b81067bbf9a6aa
Nishant Srivastava <nisrulz@gmail.com>
2018-02-10 19:13:21 +0530

    added: grid_layout

---
+++ grid_layout/README.md
@@ -0,0 +1,8 @@
+# grid_layout
+
+Using Grid Layout in Flutter app
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](http://flutter.io/).
---
+++ grid_layout/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new Scaffold(
+        appBar: new AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: new Text("GridView Example"),
+        ),
+        body: new Container(
+          child: new Center(child: new Text("Hello World")),
+        ),
+      ),
+    );
+  }
+}
