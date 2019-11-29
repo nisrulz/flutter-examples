5a8b0f1d8bf489583c9350a346bf2a80f809bf2f
Nishant Srivastava <nisrulz@gmail.com>
2019-11-29 03:22:08 +0100

    updated bash script to get packages

--- get_packages.sh
+++ get_packages.sh
@@ -4,7 +4,7 @@
 #  Call as
 #  ./get_packages.sh
 
-echo "# Updating to packages..."
+echo "  ‣  Updating packages..."
 
 # Iterate over each sub-directory inside the current directory
 for DIR in ./*;
@@ -12,12 +12,18 @@ do
 	# Check if pubspec.yaml file exists inside the $DIR directory
 	# If it does then it is a Flutter project
 	if [ -f "$DIR/pubspec.yaml" ]; then
-		# Update the packages in the project
-		echo ""
-		echo ">>>> Get packages for " "$DIR"
+		# Navigate into the sub directory
+		cd "$DIR"
+
 		# Run `flutter packages get` command inside the sub-directory i.e Flutter project
-		(cd "$DIR" && flutter packages get);
-		echo ""
-		echo ">>>> Done."
+		flutter packages get | grep "FAILED"
+
+		# Print the name of the sub directory when done
+		echo "$DIR" | awk -F'/' '{print $2}' | xargs -I{} echo "      ↪️  {} ✔️"
+
+		# Go back to parent directory
+		cd ../
 	fi
 done
+
+echo "  ✔️  Done."
