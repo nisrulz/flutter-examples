0f53d4cc16518bf3e7c0d86f8add908dbca7c873
Viral Vaghela <34627404+viralvaghela@users.noreply.github.com>
2020-08-19 03:00:52 +0530

    added InteractiveViewer (#55)

---
+++ using_interactiveviewer/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("using InteractiveViewer"),
+        ),
+        body: InteractiveViewer(
+          boundaryMargin: EdgeInsets.all(100.0),
+          minScale: 0.1,
+          maxScale: 1.6,
+          child: Center(
+            child: FlutterLogo(
+              size: 90,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
