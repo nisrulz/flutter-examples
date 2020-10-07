8f8917072d6907111188c113ac91c5a2df821f74
Rishika-7 <rishikajain028@gmail.com>
2020-10-07 17:10:51 +0530

    Made all requested changes

--- bottom_sheet/test/widget_test.dart
+++ bottom_sheet/test/widget_test.dart
@@ -5,26 +5,22 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:project/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('Checks on tap dismissal of bottom sheet', (WidgetTester tester) async {
+    // Build the app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('BottomSheet'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    await tester.tap(find.text('BottomSheet'));
+    await tester.pump(); 
+    await tester.pump(const Duration(seconds: 1)); 
+    await tester.pump(const Duration(seconds: 1)); 
+    expect(find.text('BottomSheet'), findsNothing);
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
   });
 }
