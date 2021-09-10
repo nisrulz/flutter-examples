3f9712bee964fa53440f02be85e67acb89b090c6
Harry Ryder <harrymryder@gmail.com>
2021-09-10 09:27:42 +0100

    Added responsive button with scale transform on tap down and tap up

--- responsive_button/lib/main.dart
+++ responsive_button/lib/main.dart
@@ -31,7 +31,99 @@ class MyHomePage extends StatelessWidget {
         title: Text(title),
       ),
       body: const Center(
-        child: Text('Hello world'),
+        child: ResponsiveButton(
+          text: 'Press me',
+          color: Colors.blue,
+        ),
+      ),
+    );
+  }
+}
+
+class ResponsiveButton extends StatefulWidget {
+  const ResponsiveButton({
+    Key? key,
+    this.text = 'Press me',
+    this.color = const Color(0xFFFFE306),
+  }) : super(key: key);
+
+  final String text;
+  final Color color;
+
+  @override
+  _ResponsiveButtonState createState() => _ResponsiveButtonState();
+}
+
+class _ResponsiveButtonState extends State<ResponsiveButton>
+    with SingleTickerProviderStateMixin {
+  late double _scale;
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 100),
+      lowerBound: 0.0,
+      upperBound: 0.1,
+    )..addListener(() {
+        setState(() {});
+      });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.removeListener(() {});
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _tapDown(TapDownDetails details) {
+    _controller.forward();
+  }
+
+  void _tapUp(TapUpDetails details) {
+    _controller.reverse();
+    // Add widget.onPressed here
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _scale = 1 - _controller.value;
+
+    return GestureDetector(
+      onTapDown: _tapDown,
+      onTapUp: _tapUp,
+      child: Transform.scale(
+        scale: _scale,
+        child: Container(
+          height: 64,
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          width: double.infinity,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: widget.color,
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.grey,
+                offset: Offset(0, 2.0), //(x,y)
+                blurRadius: 2.0,
+              ),
+            ],
+          ),
+          alignment: Alignment.center,
+          child: Text(
+            widget.text,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
       ),
     );
   }
