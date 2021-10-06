f0651a2b44491591eea7e365448f9989c3fb5bab
RohanSengupta326 <senguptarohan34@gmail.com>
2021-10-06 14:08:03 +0530

    recreating

--- MealsApp/android/app/src/main/kotlin/github/nisrulz/mealsapp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package github.nisrulz.MealsApp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- MealsApp/lib/models/category.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Category {
-  final String id;
-  final String title;
-  final Color color;
-
-  const Category({required this.id, required this.title, this.color = Colors.purple});
-}
