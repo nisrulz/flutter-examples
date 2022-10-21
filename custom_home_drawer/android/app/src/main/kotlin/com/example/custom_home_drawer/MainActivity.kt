ab18057b0447396d7122bbff3804044d6d379303
Vasu Gajjar <vasugajjar1@gmail.com>
2022-10-21 12:01:55 +0530

    Added new example custom_home_drawer

---
+++ custom_home_drawer/android/app/src/main/kotlin/com/example/custom_home_drawer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.custom_home_drawer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ custom_home_drawer/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:custom_home_drawer/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
